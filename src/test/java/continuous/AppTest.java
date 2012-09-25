34e3cc16995d4264723df66740eccd604aa00d4e
yukung <yukung.i@gmail.com>
2012-09-26 00:34:41 +0900

    Add sample test case.

---
+++ src/test/java/continuous/AppTest.java
@@ -0,0 +1,15 @@
+package continuous;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+	
+	@Test
+	public void test() {
+		assertThat(1, is(1));
+	}
+	
+}
