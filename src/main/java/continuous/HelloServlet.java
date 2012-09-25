2874a6c3a9794689171eb3d69c20e9ae6dfee6ba
yukung <yukung.i@gmail.com>
2012-09-25 17:16:48 +0900

    First commit. Configure the project.

---
+++ src/main/java/continuous/HelloServlet.java
@@ -0,0 +1,44 @@
+package continuous;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+public class HelloServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	
+	
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public HelloServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Hello, console!");
+		request.getRequestDispatcher("/").forward(request, response);
+	}
+	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+			IOException {
+		// TODO Auto-generated method stub
+	}
+	
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<c:set var="name" value="Tomcat"/>
+
+<html>
+<body>
+<h1>Hello, world!</h1>
+<p><c:out value="${name}" /></p>
+</body>
+</html>
