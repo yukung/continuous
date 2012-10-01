3a304a3e4abc9998018f66ad2c49a7e6da9e5951
yukung <yukung.i@gmail.com>
2012-10-01 17:55:04 +0900

    Add comment implementation strategy.

--- src/main/java/continuous/service/impl/UserServiceImpl.java
+++ src/main/java/continuous/service/impl/UserServiceImpl.java
@@ -12,6 +12,10 @@ public class UserServiceImpl implements UserService {
 	public UserServiceImpl(UserDao userDao) {
 		// TODO とりあえずDIは使わないで作るので、DAOファクトリクラスを作って設定する感じか
 		// TODO ファクトリクラス導入したら、引数のDAOは要らなくて、ファクトリクラスから設定
+		// this.userDao = DaoFactory.create(daoName); みたいな
+		// https://github.com/yukung/tasklet/blob/master/src/main/java/org/yukung/tasklet/factory/DaoFactory.java
+		// の各DAOを返すメソッドを抽象クラスかインタフェースを返すようなメソッド定義にして一つにまとめて、
+		// 引数で生成するインスタンスを判断するようにする。
 		this.userDao = userDao;
 	}
 	
