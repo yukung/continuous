b7752809992134665356bf98b83a501ae81e8001
yukung <yukung.i@gmail.com>
2013-01-28 19:48:25 +0900

    Add service class that provides user-related operations.

---
+++ src/main/java/continuous/service/UserService.java
@@ -0,0 +1,12 @@
+package continuous.service;
+
+/**
+ * ユーザ情報を操作するサービスクラスです.
+ *
+ * @since TODO for ikeda_yusuke
+ * @version $Id$
+ * @author ikeda_yusuke
+ */
+public interface UserService {
+	
+}
