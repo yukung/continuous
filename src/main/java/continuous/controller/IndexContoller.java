137fa3d132b755c8e8d2074b18fc854c9f4a928b
yukung <yukung.i@gmail.com>
2013-01-28 18:16:26 +0900

    Add controller class of index page.

---
+++ src/main/java/continuous/controller/IndexContoller.java
@@ -0,0 +1,21 @@
+package continuous.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * インデックスページのコントローラです。
+ * 
+ * @since 0.0.1 for yukung
+ * @version $Id$
+ * @author yukung
+ */
+public class IndexContoller extends Controller {
+	
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) {
+		// TODO リクエスト情報から、ユーザのログイン判定を行なって、ログインページへの遷移またはホーム画面への遷移処理を行う
+		return null;
+	}
+	
+}
