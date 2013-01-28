6d5d98c1d972383e47a2ef9b22513d52ec70a798
yukung <yukung.i@gmail.com>
2013-01-28 18:18:18 +0900

    Add a DAO class to get the user-related data.

---
+++ src/main/java/continuous/dao/UserDao.java
@@ -0,0 +1,56 @@
+package continuous.dao;
+
+import java.util.List;
+
+import continuous.entity.User;
+
+/**
+ * データソースからユーザ関連の情報を取得するクラスです.
+ * 
+ * @since 0.0.1 for yukung
+ * @version $Id$
+ * @author yukung
+ */
+public interface UserDao {
+	
+	/**
+	 * ユーザ情報を削除します.
+	 * 
+	 * @param user ユーザ情報
+	 * @since 0.0.1
+	 */
+	public void delete(User user);
+	
+	/**
+	 * 指定したIDでユーザ情報を取得します.
+	 * 
+	 * @param id ユーザID
+	 * @return ユーザ情報
+	 * @since 0.0.1
+	 */
+	public User find(long id);
+	
+	/**
+	 * ユーザ情報を全て取得します.
+	 * 
+	 * @return 全てのユーザ情報
+	 * @since 0.0.1
+	 */
+	public List<User> findAll();
+	
+	/**
+	 * ユーザ情報を作成します.
+	 * 
+	 * @param user ユーザ情報
+	 * @since 0.0.1
+	 */
+	public void insert(User user);
+	
+	/**
+	 * ユーザ情報を更新します.
+	 * 
+	 * @param user ユーザ情報
+	 * @since 0.0.1
+	 */
+	public void update(User user);
+}
